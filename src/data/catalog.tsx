ecd485ca96a7e204753a0333faccc4a77ca0c19b
COCO SHAYAN <shayanzamiri.class@gmail.com>
2025-09-10 16:03:00 +0330

    sticky menu

--- src/data/catalog.tsx
+++ src/data/catalog.tsx
@@ -1,12 +1,12 @@
 export const catalog = [
   {
-    image: "/images/1.jpg",
+    image: "/images/1_1.jpg",
     text: "در این بخش شما تازه وارد بازی شدین. فارسی تایپ کردن با کیبورد من خیلی سخته",
   },
-  { image: "/images/2.jpg", text: "درس اول: ..." },
-  { image: "/images/3.jpg", text: "درس اول: ..." },
-  { image: "/images/4.jpg", text: "درس اول: ..." },
-  { image: "/images/5.jpg", text: "درس اول: ..." },
-  { image: "/images/6.jpg", text: "درس اول: ..." },
-  { image: "/images/7.jpg", text: "درس اول: ..." },
+  { image: "/images/1_2.jpg", text: "درس اول: ..." },
+  { image: "/images/1_3.jpg", text: "درس اول: ..." },
+  { image: "/images/1_4.jpg", text: "درس اول: ..." },
+  { image: "/images/1_5.jpg", text: "درس اول: ..." },
+  { image: "/images/2_1.jpg", text: "درس اول: ..." },
+  { image: "/images/2_2.jpg", text: "درس اول: ..." },
 ];
