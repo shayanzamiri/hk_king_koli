ea3409564208e6498b85c7bbdc2fd8f3fafd01c7
COCO SHAYAN <shayanzamiri.class@gmail.com>
2025-09-21 01:38:57 +0330

    next and previous button created

--- src/data/catalog.ts
+++ src/data/catalog.ts
@@ -5,19 +5,24 @@ export const catalogs = {
     data: [
       {
         image: "/images/first/first_1.jpg",
-        text: "",
+        text: `انتخاب یک شهر برای پایگاه تنها نقطه شروع اکتشاف است.
+
+شما ممکن است به مناطق محبوب تمایل بیشتری داشته باشید اما رقابت در فعالیت‌های رقابتی که بعدا در آنجا بوجود می آید به طور قابل توجهی بیشتر خواهد بود.
+
+برای انتخاب شهری به جز نیویورک روی دکمه Change کلیک کنید
+`,
       },
       {
         image: "/images/first/first_2.jpg",
-        text: "",
+        text: `در این قسمت شما میتوانید از روی نقشه شهر مورد علاقه خود را انتخاب کنید. یا میتوانید بر روی دکمه Other کلیک کنید.`,
       },
       {
         image: "/images/first/first_3.jpg",
-        text: "",
+        text: `زمانی که بر روی دکمه Other  کلیک می کنید در قسمت بالا میتوانید نام شهر مورد علاقه خود را به انگلیسی سرچ کنید و سپس آن را انتخاب کنید. مثلا من Berlin را سرچ کرده و سپس انتخاب می کنم.`,
       },
       {
         image: "/images/first/first_4.jpg",
-        text: "",
+        text: `زمانی که نام شهر مورد نظر خود را در این قسمت مشاهده کردید سپس بر روی دکمه تایید کلیک کنید.`,
       },
     ],
   },
@@ -26,12 +31,32 @@ export const catalogs = {
     title: "شناسایی کلیات",
     data: [
       {
-        image: "/images/1_1.jpg",
-        text: "",
+        image: "/images/exploration/exploration_1.jpg",
+        text: `هر بار بر روی این مستطیل کلیک می کنید یک بار عمل کاوش کردن را انجام میدهید.`,
       },
       {
-        image: "/images/1_1.jpg",
-        text: "",
+        image: "/images/exploration/exploration_2.jpg",
+        text: `به هر کدام از این بخش هایی که با نارنجی دورش خط کشیدم یک محل گویند. در هر محل شما میتوانید کاوش بخصوص آن محل را انجام دهید.`,
+      },
+      {
+        image: "/images/exploration/exploration_3.jpg",
+        text: `به این بخش که دورش خط کشیدم یک اتاق گویند و نام این اتاق نیز در بالا سمت چپ آن نوشته شده است. برای مثال نام این اتاق Kitchen میباشد به معنای آشپزخانه.`,
+      },
+      {
+        image: "/images/exploration/exploration_4.jpg",
+        text: `هر محل برای کامل شدن باید ۱۵ بار کاوش شود. شما هم میتوانید ۱۵ بار این عمل را انجام دهید هم میتوانید بر روی بخش انتهایی محل که یک دایره هست کلیک کنید و سپس کاوش را انجام دهید. در این صورت میزان انرژی لازم برای ۱۵ بار کاوش کردن این محل را از شما میگیرد و آن را کامل میکند.`,
+      },
+      {
+        image: "/images/exploration/exploration_5.jpg",
+        text: ``,
+      },
+      {
+        image: "/images/exploration/exploration_6.jpg",
+        text: ``,
+      },
+      {
+        image: "/images/exploration/exploration_7.jpg",
+        text: ``,
       },
     ],
   },
@@ -41,11 +66,11 @@ export const catalogs = {
     data: [
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
     ],
   },
@@ -55,11 +80,11 @@ export const catalogs = {
     data: [
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
     ],
   },
@@ -69,11 +94,11 @@ export const catalogs = {
     data: [
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
     ],
   },
@@ -83,11 +108,11 @@ export const catalogs = {
     data: [
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
     ],
   },
@@ -97,11 +122,11 @@ export const catalogs = {
     data: [
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
     ],
   },
@@ -111,11 +136,11 @@ export const catalogs = {
     data: [
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
     ],
   },
@@ -125,11 +150,11 @@ export const catalogs = {
     data: [
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
     ],
   },
@@ -139,11 +164,11 @@ export const catalogs = {
     data: [
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
     ],
   },
@@ -153,11 +178,11 @@ export const catalogs = {
     data: [
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
       {
         image: "/images/1_1.jpg",
-        text: "",
+        text: ``,
       },
     ],
   },
