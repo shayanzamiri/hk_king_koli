92f0c4fe3d7b6f7c97f73b51aa81991e79e38440
COCO SHAYAN <shayanzamiri.class@gmail.com>
2025-09-25 02:14:41 +0330

    menu titles complete

--- src/data/catalog.ts
+++ src/data/catalog.ts
@@ -1,5 +1,5 @@
 export const catalogs = {
-  first: {
+  First: {
     nav: "City Selection",
     title: "انتخاب اولین شهر",
     data: [
@@ -26,7 +26,7 @@ export const catalogs = {
       },
     ],
   },
-  exploration: {
+  Exploration: {
     nav: "Exploration",
     title: "شناسایی کلیات",
     data: [
@@ -70,13 +70,9 @@ export const catalogs = {
         image: "/images/2_exploration/exploration_10.jpg",
         text: `نام این بخش ابزار ( Tools ) میباشد. نماد مهندسی و سازندگی.`,
       },
-      {
-        image: "/images/2_exploration/exploration_11.jpg",
-        text: `این بخش نیز آجیل ( Nuts ) میباشد نماد غذا. البته در بین کاربران فارسی زبان به بلوط و فندق هم معروف است. ما نیز اینجا به آن بلوط میگوییم.`,
-      },
     ],
   },
-  research: {
+  Research: {
     nav: "Types of Research",
     title: "انواع کاوش",
     data: [
@@ -94,37 +90,178 @@ export const catalogs = {
       },
       {
         image: "/images/3_research/research_4.jpg",
-        text: `زمانی که یک بخش ( Section ) را ۱۵ بار کاوش کنید شما آن بخش را کامل کرده اید و یک تیک سبز به نشانه تکمیل در گوشه سمت راست آن برای همیشه قرار میگیرد.`,
+        text: `زمانی که یک بخش ( Section ) را ۱۵ بار کاوش کنید شما آن بخش را کامل کرده اید و یک تیک سبز به نشانه تکمیل در گوشه سمت راست آن برای همیشه قرار میگیرد. الان من اولین بخش این اتاق UNLOCK رو کامل کردم و همانطور که مشاهده میکنید  دسترسی به دیگر بخشهای این اتاق رو به من داد.`,
       },
       {
         image: "/images/3_research/research_5.jpg",
-        text: `هر کاوش ورودی ها و خروجی هایی دارد.سمت چپ این مثلث زرد ورودیهای و سمت راست آن خروجی ها هستند. اولین کاوش در هر اتاق ( UNLOCK ) ورودی فقط انرژی میگیرد و خروجی به شما پول رایج میدهد. `,
+        text: `کاوش کردن این بخش از شما ورودی یک عدد Rusty Chain ( زنجیر زنگ زده ) میخواد و به شما خروجی تعدادی بلوط و پول رایج میدهد.`,
       },
       {
         image: "/images/3_research/research_6.jpg",
-        text: `هر کاوش ورودی ها و خروجی هایی دارد.سمت چپ این مثلث زرد ورودیهای و سمت راست آن خروجی ها هستند. اولین کاوش در هر اتاق ( UNLOCK ) ورودی فقط انرژی میگیرد و خروجی به شما پول رایج میدهد. `,
+        text: `خروجی بعضی از بخشها بیشتر از چیزی است که اول دیده میشود. مثلا کاوش کردن این بخش به شما پول رایج هم میدهد ولی شما برای دیدن تمام خروجی باید انگشت خود را روی اون مثلث زرد بگذارید و به سمت چپ بکشید تا ادامه خروجی نمایان شود.`,
       },
       {
-        image: "/images/3_research/research_2.jpg",
-        text: `هر کاوش ورودی ها و خروجی هایی دارد.سمت چپ این مثلث زرد ورودیهای و سمت راست آن خروجی ها هستند. اولین کاوش در هر اتاق ( UNLOCK ) ورودی فقط انرژی میگیرد و خروجی به شما پول رایج میدهد. `,
+        image: "/images/3_research/research_7.jpg",
+        text: `کاوش کردن این بخش ورودی یک عدد Simple Blueprint ( طرح اولیه ساده ) میگیرد و خروجی به شما ابزار و پول رایج میدهد. البته الان پول رایج دیده نمیشه و شما باید مثلث زرد رو با انگشت به سمت چپ بکشید تا دیده بشه .`,
       },
       {
-        image: "/images/3_research/research_2.jpg",
-        text: `هر کاوش ورودی ها و خروجی هایی دارد.سمت چپ این مثلث زرد ورودیهای و سمت راست آن خروجی ها هستند. اولین کاوش در هر اتاق ( UNLOCK ) ورودی فقط انرژی میگیرد و خروجی به شما پول رایج میدهد. `,
+        image: "/images/3_research/research_8.jpg",
+        text: `این ها هم ۲ بخش دیگر از یک اتاق هستند که برای کاوش کردن ورودی فقط انرژی میگیرند و خروجی منابع متنوع میدهند. به نام بخشها دقت کنید. بخشهای مختلف نسبت به رنگ ساختمان و اسم ساختمان و اسم اتاق متفاوت هستند پس ساختمانهای خود را هوشمندانه انتخاب کنید.`,
+      },
+      {
+        image: "/images/3_research/research_8.jpg",
+        text: `امکانهایی هستند که که برای هر بار کاوش از شما منابعی برای ورودی کاوش میخواهند و به شما منابع دیگری خروجی میدهند.`,
+      },
+      {
+        image: "/images/3_research/research_8.jpg",
+        text: `امکان هایی هستند که کاوش کردن آنها از شما منابعی جهت ورودی نمیخواهد و صرفا با صرف انرژی به شما الماس خروجی میدهند.`,
+      },
+      {
+        image: "/images/3_research/research_8.jpg",
+        text: `زمانی که یک امکان را کامل کاوش میکنیم ( ۱۵ بار ) در این زمان شما میتوانید دوباره آن امکان را کاوش کنید. برای این کار بر روی دکمه انتهای سمت راست کاوش کلید کنید سپس از شما می پرسد این امکان را چند بار میخواهی کاوش کنی ؟ ۵۰ بار یا ۱۰ بار`,
       },
     ],
   },
-  tiers: {
+  AccNewBuildings: {
+    nav: "Acces New Buildings",
+    title: "دسترسی به ساختمانهای جدید",
+    data: [
+      {
+        image: "/images/4_access_new_buildings/",
+        text: `شما در این بخش میتوانید ظرفیت کل ساختمانهایی که میتواند کاوش کنید را مشاهده کنید. ما در این راهنما به این عدد میگوییم ظرفیت ساختمانها. ظرفیت ساختمانها را میتوانید با روشهایی که پایین تر خدمتتون توضیح میدم افزایش دهید. در سمت چپ آن تعداد مجموع ساختمان هایی که تا الان کاوش کردید را مشاهده میکنید. شما میتوانید اگر ظرفیت ساختمان دارید با انتخاب یک ساختمان جدید در یکی از منطقه های یکی از شهرهایتان و تایید کاوش آنرا کاوش کنید.`,
+      },
+      {
+        image: "/images/4_access_new_buildings/",
+        text: `یکی از راه ها برای افزایش ظرفیت ساختمانها این هست که در مرکز وارد این ساختمان بشید. این ساختمان مرکز گسترش امکان ساخت و ساز کلی بازی شماست که ما در این راهنما به آن میگوییم `,
+      },
+      {
+        image: "/images/4_access_new_buildings/",
+        text: `این بخش از ساختمان فلان را هر بار که ارتقا بدید یک ساختمان به ظرفیت ساختمانهای شما اضافه می کند`,
+      },
+      {
+        image: "/images/4_access_new_buildings/",
+        text: `یکی دیگه از راه های افضایش ظرفیت ساختمانها در این ساختمان شما قرار دارد. این ساختمان برای کنترل بخشهای مربوط به پبت ها (Pit) هستند که در بخش پیت از منو میتوانید کامل راجب آنها مطالعه کنید `,
+      },
+      {
+        image: "/images/4_access_new_buildings/",
+        text: `این بخشها را هر چند باری که ارتقا دهید به شما میگوید که ارتقای بعدی ۵ عدد به ظرفیت ساختمانهای شما اضافه میکند.`,
+      },
+      {
+        image: "/images/4_access_new_buildings/",
+        text: `این بخشها را هر چند باری که ارتقا دهید به شما میگوید که ارتقای بعدی ۵ عدد به ظرفیت ساختمانهای شما اضافه میکند.`,
+      },
+      {
+        image: "/images/4_access_new_buildings/",
+        text: `این بخشها را هر چند باری که ارتقا دهید به شما میگوید که ارتقای بعدی ۵ عدد به ظرفیت ساختمانهای شما اضافه میکند.`,
+      },
+      {
+        image: "/images/4_access_new_buildings/",
+        text: `این ساختمان مربوط به رویدادهای بازی میباشد. برای ارتقای بعضی از قابلیت ها به بخش رویدادها از منو مراجعه کنید. در آن قسمت هر رویدادی که درحال حاظر در حال انجام است را میتوانید به طور کامل مطالعه کنید.`,
+      },
+      {
+        image: "/images/4_access_new_buildings/",
+        text: `برای مثال این رویداد با هر بار ارتقا به شما ۱ عدد ظرفیت ساختمان میدهد.`,
+      },
+    ],
+  },
+  Cartels: {
+    nav: "Cartels",
+    title: "کارتل ها",
+    data: [
+      {
+        image: "/images/5_cartels/",
+        text: ``,
+      },
+      {
+        image: "/images/5_cartels/",
+        text: ``,
+      },
+      {
+        image: "/images/5_cartels/",
+        text: ``,
+      },
+      {
+        image: "/images/5_cartels/",
+        text: ``,
+      },
+    ],
+  },
+  CombatSystem: {
+    nav: "Combat System",
+    title: "سیستم جنگ ها",
+    data: [
+      {
+        image: "/images/6_combat_system/",
+        text: ``,
+      },
+    ],
+  },
+  Tiers: {
     nav: "Tiers",
     title: "رده بندی ها",
     data: [
       {
-        image: "/images/1_1.jpg",
-        text: ``,
+        image: "/images/7_tiers/",
+        text: `در این بازی ما کلا تمام رده بندی هایی که داریم هم با استفاده از عدد و هم رنگ مشخص میشود. همیشه سطح ۱ با رنگ سفید و سطح ۲ با رنگ سبز و سطح ۳ با رنگ آبی و سطح ۴ با رنگ بنفش و سطح ۵ با رنگ نارنجی نشان داده میشود. `,
       },
       {
-        image: "/images/1_1.jpg",
-        text: ``,
+        image: "/images/7_tiers/",
+        text: `هر کدام از این سطح ها اسم هم دارند. 
+        سطح ۱ به نام معمولی ( common )
+        و سطح ۲ به اسم غیر معمولی ( uncommon ) 
+        و سطح ۳ به اسم خاص ( epic ) 
+        و سطح ۴ به اسم خفن ( legend ) 
+        و سطح ۵ به اسم خدا ( god )`,
+      },
+      {
+        image: "/images/7_tiers/",
+        text: `این سطح بندی در ساختمانها اندکی متفاوت میباشد به این دلیل که بین سطح ۴ و ۵ یک سطح ۴ پلاس نیز وجود دارد. همچنین بعد از سطح ۵ نیز یک سطح ۵ پلاس نیز وجود دارد`,
+      },
+      {
+        image: "/images/7_tiers/",
+        text: `سطح بندی در بین آیتم های فروشگاه نیز بر اساس همین سطح بندی با نشان دادن رنگها و نوشتن اسم آنها مشخص شده است`,
+      },
+      {
+        image: "/images/7_tiers/",
+        text: `منابع`,
+      },
+      {
+        image: "/images/7_tiers/",
+        text: `در بیزنس ها`,
+      },
+      {
+        image: "/images/7_tiers/",
+        text: `در بیزنس ها`,
+      },
+      {
+        image: "/images/7_tiers/",
+        text: `در کارتلها`,
+      },
+      {
+        image: "/images/7_tiers/",
+        text: `در کارتلها`,
+      },
+      {
+        image: "/images/7_tiers/",
+        text: `در فروشگاه`,
+      },
+      {
+        image: "/images/7_tiers/",
+        text: `در ساختمان ها`,
+      },
+      {
+        image: "/images/7_tiers/",
+        text: `در ساختمان ها از بیرون`,
+      },
+      ,
+      {
+        image: "/images/7_tiers/",
+        text: `در ساختمان ها از بیرون`,
+      },
+      ,
+      {
+        image: "/images/7_tiers/",
+        text: `در ساختمان ها از بیرون`,
       },
     ],
   },
@@ -133,25 +270,7 @@ export const catalogs = {
     title: "ارتقا ساختمان",
     data: [
       {
-        image: "/images/1_1.jpg",
-        text: ``,
-      },
-      {
-        image: "/images/1_1.jpg",
-        text: ``,
-      },
-    ],
-  },
-  NewBuildings: {
-    nav: "Access to New Buildings",
-    title: "ساختمان جدید",
-    data: [
-      {
-        image: "/images/1_1.jpg",
-        text: ``,
-      },
-      {
-        image: "/images/1_1.jpg",
+        image: "/images/8_building_renovation/",
         text: ``,
       },
     ],
@@ -161,67 +280,221 @@ export const catalogs = {
     title: "منابع",
     data: [
       {
-        image: "/images/1_1.jpg",
+        image: "/images/9_resources/",
         text: ``,
       },
       {
-        image: "/images/1_1.jpg",
+        image: "/images/9_resources/",
+        text: ``,
+      },
+    ],
+  },
+  HamsterBase: {
+    nav: "Base",
+    title: "مرکز",
+    data: [
+      {
+        image: "/images/10_hamster_base/",
+        text: ``,
+      },
+      {
+        image: "/images/10_hamster_base/",
         text: ``,
       },
     ],
   },
   Investment: {
-    nav: "Special Building [Investment]",
-    title: "ساختمان ویژه",
+    nav: "Special Buildings [Investment]",
+    title: "ساختمانهای ویژه {سرمایه گزاری}",
     data: [
       {
-        image: "/images/1_1.jpg",
+        image: "/images/11_investment/",
         text: ``,
       },
       {
-        image: "/images/1_1.jpg",
+        image: "/images/11_investment/",
         text: ``,
       },
     ],
   },
-  Influence: {
-    nav: "Influence Points",
-    title: "نفوذ",
+  PassiveIncome: {
+    nav: "Passive Income",
+    title: "درآمد ساعتی",
     data: [
       {
-        image: "/images/1_1.jpg",
+        image: "/images/12_passive_income",
         text: ``,
       },
       {
-        image: "/images/1_1.jpg",
+        image: "/images/12_passive_income",
         text: ``,
       },
     ],
   },
-  CombatCartels: {
-    nav: "Combat Cartels",
-    title: "همسترهای جنگی",
+  Rumors: {
+    nav: "Rumors",
+    title: "شایعات",
     data: [
       {
-        image: "/images/1_1.jpg",
+        image: "/images/13_rumors/",
         text: ``,
       },
       {
-        image: "/images/1_1.jpg",
+        image: "/images/13_rumors/",
         text: ``,
       },
     ],
   },
-  Base: {
-    nav: "First things",
-    title: "مرکز",
+  Bussiness: {
+    nav: "Bussiness",
+    title: "بیزینس ها- کسب و کارها",
     data: [
       {
-        image: "/images/1_1.jpg",
+        image: "/images/14_bussiness/",
         text: ``,
       },
       {
-        image: "/images/1_1.jpg",
+        image: "/images/14_bussiness/",
+        text: ``,
+      },
+    ],
+  },
+  Pit: {
+    nav: "Pit",
+    title: "پیت ها",
+    data: [
+      {
+        image: "/images/15_pit/",
+        text: ``,
+      },
+      {
+        image: "/images/15_pit/",
+        text: ``,
+      },
+    ],
+  },
+  Boss: {
+    nav: "Boss",
+    title: "رئیس",
+    data: [
+      {
+        image: "/images/16_boss/",
+        text: ``,
+      },
+      {
+        image: "/images/16_boss/",
+        text: ``,
+      },
+    ],
+  },
+  Clan: {
+    nav: "Clan",
+    title: "کلن",
+    data: [
+      {
+        image: "/images/17_clan/",
+        text: ``,
+      },
+      {
+        image: "/images/17_clan/",
+        text: ``,
+      },
+    ],
+  },
+  shop: {
+    nav: "Shops",
+    title: "فروشگاه ها",
+    data: [
+      {
+        image: "/images/18_shop/",
+        text: ``,
+      },
+      {
+        image: "/images/18_shop/",
+        text: ``,
+      },
+    ],
+  },
+  HeadOffice: {
+    nav: "Head Office",
+    title: "دفتر مرکزی",
+    data: [
+      {
+        image: "/images/19_head_office/",
+        text: ``,
+      },
+      {
+        image: "/images/19_head_office/",
+        text: ``,
+      },
+    ],
+  },
+  StrategyReseources: {
+    nav: "Strategy Resources",
+    title: "استراتژی منابع",
+    data: [
+      {
+        image: "/images/20_strategy_resources/",
+        text: ``,
+      },
+      {
+        image: "/images/20_strategy_resources/",
+        text: ``,
+      },
+    ],
+  },
+  StrategyCartels: {
+    nav: "Strategy Cartles",
+    title: "استراتژی کارتل ها",
+    data: [
+      {
+        image: "/images/21_strategy_cartels/",
+        text: ``,
+      },
+      {
+        image: "/images/21_strategy_cartels/",
+        text: ``,
+      },
+    ],
+  },
+  StrategyPit: {
+    nav: "Strategy Pit",
+    title: "استراتژی پبت",
+    data: [
+      {
+        image: "/images/22_strategy_pit/",
+        text: ``,
+      },
+      {
+        image: "/images/22_strategy_pit/",
+        text: ``,
+      },
+    ],
+  },
+  StrategyGem: {
+    nav: "Strategy Gem Guild Gold",
+    title: "استراتژی الماس ها و گیلد گلد",
+    data: [
+      {
+        image: "/images/23_strategy_gem_guild_gold/",
+        text: ``,
+      },
+      {
+        image: "/images/23_strategy_gem_guild_gold/",
+        text: ``,
+      },
+    ],
+  },
+  GiftPack: {
+    nav: "Gift Pack",
+    title: "بسته های هدیه",
+    data: [
+      {
+        image: "/images/24_gift_pack/",
+        text: ``,
+      },
+      {
+        image: "/images/24_gift_pack/",
         text: ``,
       },
     ],
